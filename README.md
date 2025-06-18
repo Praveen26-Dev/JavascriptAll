7aa2dfc91b1a0aaabb82deafab8c276b3f12db36
Praveen26-Dev <praveenpandey709@gmail.com>
2025-06-18 22:17:37 +0530

    after learning basics of JavaScript

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JavascriptAll
 this repository contains all js codes
+This is maintained by Praveen Pandey
---
+++ day1/index.js
@@ -0,0 +1,7 @@
+
+// Output Statements
+console.log("Hello this is awesome")
+
+// variable is named memory location
+// in js we have three types of variable i.e var , let , const
+document.write("This is document")
