a85b1561dd6bd4f2937343dd4732153b57134e00
Praveen26-Dev <praveenpandey709@gmail.com>
2025-06-18 22:29:05 +0530

    after operators of JavaScript

---
+++ day2/operator.js
@@ -0,0 +1,31 @@
+// we have 3 types of variable in javascript
+// if we use "var" then reassignment or redeclare can be possible with same variable name and the scope is global
+// and if we use "let" then redeclare is not possible but reassignment can be possible and the scope is local
+// then if we use "const" then redeclare and reassignment both cannot be possible and the scope is local
+
+// Operators -- these are special type of symbols that we use for operations and computations.
+
+// there r 3 types of operators
+// unary , binary, ternary
+// 1.Unary 
+//   example-- 1. ++,--
+
+// 2.Binary
+//    example- a)Arithmatic = +,-,*,/,%
+//             b)Assignment -> +=,-=,*=,/=
+//             c)Relational -> >,<,<=,>=, == , ===,!=
+//             d)Logical -->   !,&&,|| 
+
+// 3.ternary 
+// example-> (condition) ? true :False
+
+
+// write a program to find area of circle r=4 and print it in console;
+var r=4;
+console.log(3.14*r*r);
+
+
+var a = parseInt(prompt("enter 1 number"))
+var b = parseInt(prompt("enter 2 number"))
+
+console.log(a+b)
