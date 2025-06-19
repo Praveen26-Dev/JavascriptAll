8e7ad41804f1cf21b23a67d07862c5f76742b632
Praveen26-Dev <praveenpandey709@gmail.com>
2025-06-19 15:47:30 +0530

    After Learning conditionals in js

---
+++ day3/conditionals.js
@@ -0,0 +1,40 @@
+//1. positive and negative
+// var num = parseInt(prompt("enter a number"))
+// if(num>0){
+//     console.log("number is +ve")
+// }
+// else{
+//     console.log("number is -ve")
+// }
+
+//2. write a program to take input from the user and check number is even or odd
+
+// var num = parseInt(prompt("enter a number"))
+// if(num%2==0){
+//     console.log("number is even")
+// }
+// else{
+//     console.log("number is odd")
+// }
+
+// 3.write a program to check number is negative positive or nuetral
+// var num = parseInt(prompt("enter a number"))
+// if(num>0){
+//     console.log("number is +ve")
+// }
+// else if(num<0){
+//     console.log("number is -ve")
+// }
+// else{
+//     console.log("number is neutral")
+// }
+
+// 3.write a program to check number is odd or even if number is odd then print area of circle of that number and if number is positive then print cube of that number
+// var num = parseInt(prompt("enter a number"))
+// if(num%2!=0){ 
+//     console.log("Area circle of number "+ 3.14*num*num);
+// }
+// else{
+//     console.log("cube of number using that number " +num*num*num)
+// }
+
