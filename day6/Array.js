fcd7918c8949dc627fa3f92233ea3b0a5c9826d0
Praveen26-Dev <praveenpandey709@gmail.com>
2025-06-20 20:30:38 +0530

    After learning more on loops and basics of Arrays is js

---
+++ day6/Array.js
@@ -0,0 +1,26 @@
+// Array is a collection of multiple types of data 
+// denoted by [] 
+// ordered storage -> due to indexing -> it starts from 0
+// 
+
+// let ar = ["ram", 32, 7900.44,'B',true]
+// for(var i=0;i<5;i++){
+//     console.log(ar[i])
+// }
+
+// let ar =[1,2,3,4,5,6,7,8,9,10];
+// for(var i=0;i<10;i++){
+//     console.log(ar[i])
+// }
+// for(var i=0;i<10;i++){
+//     if(ar[i]%2==0){
+//         console.log(ar[i])
+//     }
+// }
+
+// h.w why array starts from 0
+let arr1 = [1,2,3,4,5,6,7,8,9,10];
+for(v1 of arr1){
+    console.log(v1)
+    
+}
