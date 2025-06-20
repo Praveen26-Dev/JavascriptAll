932bfb2650cce20cedb5358f6f2e7df4a3f99c04
Praveen26-Dev <praveenpandey709@gmail.com>
2025-06-20 17:09:59 +0530

    After learning the loops in js

---
+++ day4/loops.js
@@ -0,0 +1,42 @@
+
+// do While loop
+var n =1;
+do{
+    console.log(n)
+    n++;
+}while(n<11);
+
+// write a program to print table of number 
+
+// var num =prompt("Enter a number for the table");
+// var i=1;
+// do{
+//     console.log(num*i);
+//     i++;
+
+// }while(i<=10)
+
+// write a program to microsoft 15 times
+// var i=1
+// var str ="Microsoft"
+// while(i<=15){
+//     document.write(i "=" str,"<br>");
+//     i++;
+// }
+
+// write a program to print reverser table using while loop
+// var i=10;
+var num = parseInt(prompt("enter a  number"))
+while(i>0){
+    console.log(num*i)
+    i--
+
+}
+// write a program to print only odd numbers between  20 to 1
+var num=20;
+while(num>0){
+    if(num%2==1){
+    console.log(num)
+    }
+    num--;
+}
---
+++ day5/loops2.js
@@ -0,0 +1,34 @@
+//1. wap to print 1 to 10 using for loop
+// for(var n=1;n<=10;n++){
+//     console.log(n)
+// }
+// 2. take input of value from user 
+// var input =parseInt(prompt("Enter a value from 1 to 20 "))
+// if(input>=1&&input<=10){
+//     for(var i=1;i<=input;i++){
+//         console.log(i);
+//     }
+// }
+// else if(input>=11 && input<=20){
+//     for(var i=20;i>=1;i--){
+//         console.log(i)
+//     }
+// }
+// else{
+//     console.log("wrong choice")
+// }
+
+// WAP to print square of all even numbers between 20 to 1 
+// for(var i=20;i>=1;i--){
+//     if(i%2==0){
+//         console.log(i*i);
+//     }
+// }
+
+// for(var i=1;i<=5;i++){
+
+//    for(var j=1;j<=i;j++){
+//     document.write("*");
+//    }
+//    document.write("<br>")
+// }
