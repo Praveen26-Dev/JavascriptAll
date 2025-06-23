c2edd9ae42e0d45282cd06fec1b8f2e35af94ada
Praveen26-Dev <praveenpandey709@gmail.com>
2025-06-23 17:28:12 +0530

    After learning more of Arrays is js

---
+++ day7/Array2.js
@@ -0,0 +1,56 @@
+let ar = ["Praveen ",21,'bhopal',5.8]
+// console.log(ar)
+// for(let v of ar){
+//     console.log(v)
+// }
+
+// push used for adding the element from last element 
+ar.push('India')
+console.log(ar)
+
+ar.push('Mp')
+console.log(ar)
+
+// pop used for removing element from last index
+ar.pop()
+console.log(ar)
+ar.pop()
+console.log(ar)
+
+// unshift for adding value from start index
+ar.unshift('101','PP')
+console.log(ar)
+// shif used for removing element from starting index
+
+ar.shift()
+console.log(ar)
+
+// slice is used for fetching elements from any to index to that index we want 
+
+var a = ar.slice(1,3)
+console.log(a)
+
+// splice is used for removing element from middle of array
+let ar11 = ["Praveen ",21,'bhopal',5.8]
+// ar11.splice(1,3)
+// console.log(ar11)
+
+ar11.splice(2,1,'sir');
+console.log(ar11)
+
+
+// Questions based on above functions of arrays
+// take element and print table
+
+let arr=['Cy',2,3,'N'];
+let val = arr.slice(2,3)
+for(var i =1;i<=10;i++){
+    console.log(val[0]*i);
+}
+
+let arr1 = [1,3,5,2,4,10,11,15]
+var sindex = parseInt(prompt("enter the start value from array"));
+var delemel = parseInt(prompt("enter the elements you want to delete "));
+
+let a1  = arr1.splice(sindex,delemel)
+console.log(a1)
