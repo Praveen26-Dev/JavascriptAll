aee0702e4801f3df5fe5940abb0a941cc9b75645
Praveen26-Dev <praveenpandey709@gmail.com>
2025-07-08 17:26:24 +0530

    After learning about events from js

---
+++ day19/EventsFromJS.js
@@ -0,0 +1,33 @@
+// let btn = document.querySelector('#btn')
+// btn.addEventListener('click',function(){
+//     alert("Your code is perfect")
+// })
+
+// btn.addEventListener('mouseleave',function(){
+//     alert("Your code is awesome")
+// })
+
+// btn.addEventListener('mouseenter',function(){
+//     alert("Your code is good")
+// })
+btn.addEventListener('dblclick',function(){
+    alert("Your code is Above the limit")
+})
+
+let btn1 = document.querySelector('#btn1')
+btn1.addEventListener('click',()=>{
+    let wr = document.querySelector('#wrapper')
+    wr.classList.add('main')
+})
+
+let btn2 = document.querySelector('.btn2')
+btn2.addEventListener('click',()=>{
+    let wr = document.querySelector('#wrapper')
+    wr.classList.remove('main')
+})
+
+let btn3 = document.querySelector('.btn3')
+btn3.addEventListener('click',()=>{
+    let wr = document.querySelector('#wrapper')
+    wr.classList.toggle('main')
+})
