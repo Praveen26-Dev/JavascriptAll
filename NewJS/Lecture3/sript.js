99ce107050aed8a19bb556ee00588dbbe317bbd6
Praveen26-Dev <praveenpandey709@gmail.com>
2025-09-08 18:21:11 +0530

    Learning about alert , confirm and prompt

---
+++ NewJS/Lecture3/sript.js
@@ -0,0 +1,8 @@
+function input(){
+    let no1 = parseInt(prompt("Enter 1st number "))
+    let no2 = parseInt(prompt("Enter 2nd number "))
+
+    let sum = no1+no2;
+    alert("Sum is == " + sum);
+}
+input();
