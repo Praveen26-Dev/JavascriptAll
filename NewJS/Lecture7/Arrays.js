15e915cf5a657e43e431b8f9d22d872970205ffc
Praveen26-Dev <praveenpandey709@gmail.com>
2025-09-12 18:30:55 +0530

    practise of questions

---
+++ NewJS/Lecture7/Arrays.js
@@ -0,0 +1,7 @@
+function methodOfArr(){
+    let arr=[1,2,4,5,6,77];
+    arr.pop()
+    arr.pop()
+    console.log(arr[3])
+    console.log(arr)
+}
---
+++ NewJS/Lecture7/script.js
@@ -0,0 +1,4 @@
+function tempLiterals(){
+    let show = `hello this 'is' my name --> <br><h1>"Praveen"</h1>`
+    document.writeln(show)
+}
