08d0d93ef9247a8f7c5fb4246c1d30a0fa06cc05
Praveen26-Dev <praveenpandey709@gmail.com>
2025-09-19 18:37:12 +0530

    making a project using dom day2

---
+++ NewJS/Lecture11/input.js
@@ -0,0 +1,28 @@
+// let num1;
+// let num2
+// let add = ()=>{
+//     num1 =document.querySelector('#num1').value
+//     num2 =document.querySelector('#num2').value
+//     let addNum = Number(num1) + Number(num2);
+//     let h1 = document.querySelector('#h1')
+//     h1.textContent=addNum
+//     // alert("Your addition is "+addNum) 
+// }
+
+let color;
+// let changeColor=()=>{
+//     let color = document.querySelector('#color').value
+//     document.body.style.backgroundColor = color
+//     // body.style.backgroundColor=color
+// }
+
+let changeColor=()=>{
+    let color = document.querySelector('#selectOption').value
+    
+    let p = document.querySelector('#para'); 
+    p.style.color = color;
+    
+
+
+    // body.style.backgroundColor=color
+}
