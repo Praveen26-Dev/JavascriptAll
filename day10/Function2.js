d9874c71eb073abd5acbada578b4e8c3e2fe68d4
Praveen26-Dev <praveenpandey709@gmail.com>
2025-10-03 15:55:45 +0530

    practise question on functions

--- day10/Function2.js
+++ day10/Function2.js
@@ -11,71 +11,79 @@
 // type 4 -> without arg and return value
 
 
-// function demo1(){
-//     var a = 25;
-//     return a*a
-// }
-// console.log(demo1());
+function demo1(){
+    var a = 25;
+    return a*a
+}
+console.log(demo1());
 
 //H.W.->  What is ES6.
+
+// function have two categories 
+// 1. Named Function
+//    ex ---> function Demo(){}
+
+// 2.Anonymous function
+
 // Arrow Function -> if we want use one liner function 
 // denotion ==   1. ()=>{ return }
 //               2.  () => ()
             //   3.  () =>
             //   4.     =>
-let arrow = ()=>{
-    return console.log("hii")
-}
+// let arrow = ()=>{
+//     return console.log("hii")
+// }
 
-arrow()
+
+// arrow()
 
 // Arrow function with parameter
-let d = (a) =>{
-    return a*a
+// let d = (a) =>{
+//     return a*a
 
 
-}
+// }
 
-console.log(d(5))
+// console.log(d(5))
 
 // =========================== OBJECT ===================================
 
 // let comes to the object again
 
-let ob = 
-{
-    name:"keshav",
-    age:43,
-    contact:778563,
-    address:"Bhopal",
-    Location: function(){
-        alert("MP nagar")
-    },
-    country: ()=>{
-        console.log("India")
-    }
-}
+// let ob = 
+// {
+//     name:"keshav",
+//     age:43,
+//     contact:778563,
+//     address:"Bhopal",
+//     Location: function(){
+//         alert("MP nagar")
+//     },
+//     country: ()=>{
+//         console.log("India")
+//     }
+// }
 
-ob.Location()
-console.log(ob.country())
+// ob.Location()
+// console.log(ob.country())
 
 // WAP to take 3 inputs from the user and and print multiplication 
 
-var num1 = parseInt(prompt("Enter 1st number"));
-var num2  = parseInt(prompt("Enter 2st number"))
-var num3 = parseInt(prompt("Enter 3st number"))
+// var num1 = parseInt(prompt("Enter 1st number"));
+// var num2  = parseInt(prompt("Enter 2st number"))
+// var num3 = parseInt(prompt("Enter 3st number"))
 
-function multi(a,b,c){
-    return a*b*c
-}
+// function multi(a,b,c){
+//     return a*b*c
+// }
 
-console.log(multi(num1,num2,num3))
+// console.log(multi(num1,num2,num3))
 
 // write a program to take radius from the user and print the area of circle
 
-function cicle(){
- let r = parseInt(prompt("Enter radius"))
- return 3.14*r*r
-}
+// function cicle(){
+//  let r = parseInt(prompt("Enter radius"))
+//  return 3.14*r*r
+// }
 
-console.log(cicle())
+// console.log(cicle())
--- day10/Function3.js
+++ day10/Function3.js
@@ -2,22 +2,34 @@
 //  agar 1 to 5 check it for even odd
 //  agar input even hai to sum return karo sum from 1 to 15 
 // aur agar wo odd hua to console me print karo
-let n = Number(prompt("Enter a number"))
-function CheckNum(n){
-  if(n>=1&&n<=5){  
-    let sum=0;
-  if(n%2===0){
-    for(let i=1;i<=15;i++){
-        sum+=i;
-    }
-    return sum;
-  }
-  else{
-    console.log(n);
-  }
+// let n = Number(prompt("Enter a number"))
+// function CheckNum(n){
+//   if(n>=1&&n<=5){  
+//     let sum=0;
+//   if(n%2===0){
+//     for(let i=1;i<=15;i++){
+//         sum+=i;
+//     }
+//     return sum;
+//   }
+//   else{
+//     console.log(n);
+//   }
+// }
+// else{
+//     console.log("enter number from 1 to 5 ");
+// }
+// }
+// console.log(CheckNum(n));
+
+
+// write a program to take two input from the user to find the area of triangle
+
+
+function Area(){
+  let b=Number(prompt("enter base"));
+  let h=Number(prompt("enter height"));
+  return 0.5*b*h
 }
-else{
-    console.log("enter number from 1 to 5 ");
-}
-}
-console.log(CheckNum(n));
+
+console.log(Area());
