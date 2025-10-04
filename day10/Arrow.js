e3851095406965a25e7a09ff139b96273b6282d2
Praveen26-Dev <praveenpandey709@gmail.com>
2025-10-04 18:37:56 +0530

    arrow function completion

---
+++ day10/Arrow.js
@@ -0,0 +1,53 @@
+// let fun = ()=>{
+//     console.log("Praveen")
+// }
+// console.log(fun())
+
+
+
+// let fun1 = (a)=>{
+// return a+a
+// }
+
+// console.log(fun1(77))
+
+
+// let sq = ()=>{
+//     let n=Number(prompt("enter a number"));
+//     return n*n;
+// }
+// console.log(sq());
+
+let rec=()=>{
+    let l = Number(prompt("Enter length"));
+    let b = Number(prompt("Enter breadth"));
+    return l*b
+}
+
+let area = rec();
+console.log("area of reatangle "+area);
+
+// let tri=()=>{
+//     let h = Number(prompt("Enter height"));
+//     let b = Number(prompt("Enter base"));
+//     return 0.5*h*b
+// }
+
+// let ar = tri();
+// console.log("area of triangle"+ar);
+
+
+// let cb = ()=>{
+//     let n=Number(prompt("enter a number"));
+//     return n*n*n;
+// }
+// console.log(cb());
+
+
+// let cir=()=>{
+//     let r = Number(prompt("Enter radius"));
+//     return 3.14*r*r
+// }
+
+// let areaC= tri();
+// console.log("area of circle"+areaC);
