a24410e48303f241a49e4dd4398cf6bbcaa86db2
Praveen26-Dev <praveenpandey709@gmail.com>
2025-10-06 16:24:54 +0530

    learning basics of DOM

--- day11/Dom.js
+++ day11/Dom.js
@@ -3,15 +3,15 @@
 // the elements in html is known as node in dom.
 // 
 
-let a = document.getElementById('h1')
+// let a = document.getElementById('h1')
 
-console.log(h1)
+// console.log(h1)
 
 // text change
-a.textContent = "hello from js for learning DOM"
+// a.textContent = "hello from js for learning DOM"
 
-let b = document.getElementById('h2')
-b.innerHTML = "<i> Hello Sir</i>"
+// let b = document.getElementById('h2')
+// b.innerHTML = "<i> Hello Sir</i>"
 
 // the getElementById returns only single element 
 
@@ -19,9 +19,9 @@ b.innerHTML = "<i> Hello Sir</i>"
 
 // and we have to target their element by their index value
 
-let c =document.getElementsByClassName('h3')
-c[0].textContent = "someone is coming"
-c[1].textContent = "you are beautiful"
+// let c =document.getElementsByClassName('h3')
+// c[0].textContent = "someone is coming"
+// c[1].textContent = "you are beautiful"
 
 
 
@@ -29,11 +29,20 @@ c[1].textContent = "you are beautiful"
 
 
 
+let h1 = document.getElementById('h1')
+// h1.textContent = "hi"
 
+h1.innerHTML="<u>Hello</u>"
+// document.getElementById('h2').textContent = "hii praveen"
+// document.getElementById('p1').textContent = "hii praveen"
+// document.getElementById('span1').textContent = "hii praveen"
+// document.getElementById('h3').textContent = "hii praveen"
+// document.getElementById('h6').textContent = "hii praveen"
+// document.getElementById('h1').textContent = "hii praveen"
+h1.setAttribute('align','center')
+document.body.setAttribute('bgcolor','aquamarine')
 
-
-
-
+h1.removeAttribute('align')
 
 
 
