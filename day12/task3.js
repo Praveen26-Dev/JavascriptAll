8ef42417a80535b71360f19b414b3c717978287c
Praveen26-Dev <praveenpandey709@gmail.com>
2025-10-11 15:43:32 +0530

    doing a task in event

---
+++ day12/task3.js
@@ -0,0 +1,21 @@
+// let h=document.getElementsByTagName('h2')
+// console.log(h)
+// h[0].style.backgroundColor="Yellow"
+// h[1].style.backgroundColor="Blue"
+// h[2].style.backgroundColor="brown"
+// h[3].style.backgroundColor="Pink"
+// h[4].style.backgroundColor="Orange"
+// h[5].style.backgroundColor="Green"
+
+let b=document.getElementsByTagName('body')
+b.style.backgroundColor="black"
+
+
+
+let h=document.getElementsByClassName('h3')
+console.log(h)
+h[0].style.backgroundColor="Yellow"
+h[1].style.backgroundColor="Blue"
+h[2].style.backgroundColor="brown"
+h[3].style.backgroundColor="Pink"
+
--- day13/Task2.js
+++ day13/Task2.js
@@ -1,34 +1,36 @@
-// function changeText(){
-//     let div = document.getElementById("div");
-//     div.style.backgroundColor="Red"
-//     div.innerHTML="<h1>Get Lost</h1>"
-//     div.style.textAlign ="center"
+// // function changeText(){
+// //     let div = document.getElementById("div");
+// //     div.style.backgroundColor="Red"
+// //     div.innerHTML="<h1>Get Lost</h1>"
+// //     div.style.textAlign ="center"
+// // }
+// // function colorChange(){
+// //     let div = document.getElementById("div");
+// //     div.style.backgroundColor="Green"
+// //     div.innerHTML="<h1>Tap Here</h1>"
+// //     div.style.textAlign ="center"
+// // }
+
+//  let img = document.getElementById('img');
+// function mobile(){
+//     img.setAttribute('src','https://m.media-amazon.com/images/I/41X9qNxoJKL._SY300_SX300_QL70_FMwebp_.jpg')
+//     img.style.height="400px"
+//     img.style.width="400px"
 // }
-// function colorChange(){
-//     let div = document.getElementById("div");
-//     div.style.backgroundColor="Green"
-//     div.innerHTML="<h1>Tap Here</h1>"
-//     div.style.textAlign ="center"
+
+// function laptop(){
+//     img.setAttribute('src','https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGxhcHRvcHxlbnwwfHwwfHx8MA%3D%3D')
+//     img.style.height="400px"
+//     img.style.width="400px"
 // }
 
- let img = document.getElementById('img');
-function mobile(){
-    img.setAttribute('src','https://m.media-amazon.com/images/I/41X9qNxoJKL._SY300_SX300_QL70_FMwebp_.jpg')
-    img.style.height="400px"
-    img.style.width="400px"
-}
+// function tablet(){
+//     img.setAttribute('src','https://images.unsplash.com/photo-1542751110-97427bbecf20?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGFibGV0fGVufDB8fDB8fHww')
+//     img.style.height="400px"
+//     img.style.width="400px"
+// }
 
-function laptop(){
-    img.setAttribute('src','https://images.unsplash.com/photo-1611186871348-b1ce696e52c9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGxhcHRvcHxlbnwwfHwwfHx8MA%3D%3D')
-    img.style.height="400px"
-    img.style.width="400px"
-}
+// let imgDiv = document.getElementById('img-div')
+// imgDiv.style.alignSelf="right"  
 
-function tablet(){
-    img.setAttribute('src','https://images.unsplash.com/photo-1542751110-97427bbecf20?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGFibGV0fGVufDB8fDB8fHww')
-    img.style.height="400px"
-    img.style.width="400px"
-}
 
-let imgDiv = document.getElementById('img-div')
-imgDiv.style.alignSelf="right"
