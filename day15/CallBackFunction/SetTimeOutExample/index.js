1b0731cb2cc9fd93eb4e51d8b1133955e48bb1b9
Praveen26-Dev <praveenpandey709@gmail.com>
2025-10-15 16:18:37 +0530

    learning about setTimout

---
+++ day15/CallBackFunction/SetTimeOutExample/index.js
@@ -0,0 +1,15 @@
+// setTimeout(()=>{
+// let div=document.getElementById("div");
+// div.innerHTML='<img src="https://images.unsplash.com/photo-1759430954379-81c2c1d908ee?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600"></img>';
+
+
+// let frm=document.getElementById('frm');
+// frm.style.display="block"
+// },3000)
+
+function callDiv(){
+    setTimeout(()=>{
+        let div=document.getElementById('div');
+        div.style.display="block"
+    },5000)
+}
