56f2faf37d8acd670156b848ed0ac7046ebfd61a
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-01 16:25:30 +0530

    array day 1

--- day6/Arr.js
+++ day6/Arr.js
@@ -1,11 +1,40 @@
 let arr = [];
 
-for (let i = 0; i < 5; i++) {
-  let value = prompt(`Enter value ${i + 1}:`);
-  arr[i] = value;
-}
+// for (let i = 0; i < 5; i++) {
+//   let value = prompt(`Enter value ${i + 1}:`);
+//   arr[i] = value;
+// }
 
-console.log("Array elements are:");
-for (let i = 0; i < 5; i++) {
-  console.log(arr[i]);
+// console.log("Array elements are:");
+// for (let i = 0; i < 5; i++) {
+//   console.log(arr[i]);
+// }
+// wap to take 10 inputs from user in array than print sum of all elements
+// let sum=0;
+// let i;
+// for ( i = 0; i < 10; i++) {
+//   let value = Number(prompt(`Enter values ${i + 1}:`));
+//   arr[i] = value;
+  
+// }
+// for(i=0;i<10;i++){
+//     sum=sum+arr[i];
+// }
+// console.log("Array elements are:");
+// for (let i = 0; i < 10; i++) {
+//   console.log(arr[i]);
+// }
+// console.log("sum is: "+sum);
+
+// wap to take 10 inputs from user and store in array than print odd numbers.
+let i;
+for ( i = 0; i < 10; i++) {
+  let value = Number(prompt(`Enter values ${i + 1}:`));
+  arr[i] = value;
+  
+}
+for(i=0;i<10;i++){
+    if(arr[i]%2==1){
+        console.log(arr[i]);
+    }
 }
