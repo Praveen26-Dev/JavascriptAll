ebec09fe6ca7d753622737f018170038e57701f3
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-03 16:16:35 +0530

    array day 2

--- day7/Array2.js
+++ day7/Array2.js
@@ -54,11 +54,11 @@
 
 // let a1  = arr1.splice(sindex,delemel)
 // console.log(a1)
-let arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-for(let v of arr){
-    if(v%2===0){
-        console.log(v*v);
-    }
-}
+// let arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+// for(let v of arr){
+//     if(v%2===0){
+//         console.log(v*v);
+//     }
+// }
 
 
