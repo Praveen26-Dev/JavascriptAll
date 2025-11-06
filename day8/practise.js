0338bfcd809a6f6adb66f66cb237399488064967
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-06 16:09:08 +0530

    Object day 2

--- day8/practise.js
+++ day8/practise.js
@@ -1,19 +1,52 @@
-let company = {
-    name:"Apple",
-    foundationYear:1976,
-    poductList:[
-        {name:"Iphone",year:2007},{name:"Ipad",year:2010}
-    ],
-    foundedBy:["Steve Jobs","Steve Wozniak"],
-     ceo:"Steve Jobs",
-    toString:function(){
-      return `<strong>name:</strong>${company.name}<br>
-               <strong>Foundation Year:</strong>${company.foundationYear}<br>
-               <strong>Founders:</strong>${company.foundedBy.join(",")}<br>
-               `;
-    }
+// let company = {
+//     name:"Apple",
+//     foundationYear:1976,
+//     poductList:[
+//         {name:"Iphone",year:2007},{name:"Ipad",year:2010}
+//     ],
+//     foundedBy:["Steve Jobs","Steve Wozniak"],
+//      ceo:"Steve Jobs",
+//     toString:function(){
+//       return `<strong>name:</strong>${company.name}<br>
+//                <strong>Foundation Year:</strong>${company.foundationYear}<br>
+//                <strong>Founders:</strong>${company.foundedBy.join(",")}<br>
+//                `;
+//     }
+
+// }
+// document.write("<h3>Company details<h3>")
+// document.write(company);
+
+// let emp={
+//     "full name":"praveen",
+//     age:20
+
+// }
+
+// console.log(emp["full name"]);
+// console.log(emp.age)
+let emp_name=prompt("Enter a name ")
+let age=prompt("Enter a age ")
+let contact=prompt("Enter a contact ")
+
+
+let emp={
+    emp_name:"",
+    emp_age:"",
+    emp_contact:""
 
 }
-document.write("<h3>Company details<h3>")
-document.write(company);
+emp.emp_name=emp_name;
+emp.emp_age=age
+emp.emp_contact=contact
 
+console.log(emp.emp_name+" "+
+emp.emp_age+" "+
+emp.emp_contact)
+for (const key in emp) {
+    console.log(key)
+    
+    
+}
+let k =Object.keys(emp)
+console.log(k)
