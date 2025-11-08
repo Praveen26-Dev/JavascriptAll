690880346984bf99ae1f6dcf7ccb9fdfb52f54f8
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-08 15:56:10 +0530

    local Storage

---
+++ day21/localStorageExample.js
@@ -0,0 +1,13 @@
+// localStorage => the storage inside the browser for storing data related to user
+// ===> key-value pair in form of String 
+// four main function
+// setItem() -> saves data into localStorage
+// getItem() -> access data
+// removeItem() -> at one time one item would be removed
+// clear() -> pura localStorage khali ker dega
+
+localStorage.setItem("username","Rahul")
+localStorage.setItem("userAge","21")
+localStorage.setItem("userAdd","Bhopal")
+localStorage.setItem("userBlg","B+")
+localStorage.setItem("userCon","9876543")
