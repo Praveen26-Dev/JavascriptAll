eae5be381c8a81d1241fd7a3f3199fc78b3c0b59
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-10 16:25:11 +0530

    String day 1 and Fetch Api

---
+++ day22/fetchExam.js
@@ -0,0 +1,4 @@
+// API -> stands for Application programming interface 
+// acts as communication medium 
+// fetch -->
+// Http Methods -->
--- day22/stringExample.js
+++ day22/stringExample.js
@@ -3,3 +3,16 @@ console.log(ename[4])
 
 console.log(ename.toUpperCase());
 
+console.log(ename.toLowerCase())
+
+let trimName = "    Tanish      "
+trimName.
+console.log(trimName.trim())
+
+// (includes , match , charAt , concat , length , index of , replace ) => these are important function
+
+
+// template literals --> ` string with  ${expression}`
+
+let st =`this is awesome`
+console.log(st)
