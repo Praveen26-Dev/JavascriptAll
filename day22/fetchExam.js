18050884c1279f46eb5cd450ed59f9098e73aee7
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-11 16:26:00 +0530

    day 1 Fetch Api

--- day22/fetchExam.js
+++ day22/fetchExam.js
@@ -2,3 +2,22 @@
 // acts as communication medium 
 // fetch -->
 // Http Methods -->
+// Promise -->
+// Aysnchronous v/s Synchronous
+
+
+async function accessFetch(){
+    let data = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let res = await data.json();
+    console.log(res)
+    
+    let result = res.map((e)=>`<tr>
+      <td>${e.userId}</td>
+      <td>${e.id}</td> 
+      <td>${e.title}</td>  
+    </tr>`).join("")
+
+    document.querySelector("#showdata").innerHTML=result
+
+}
+accessFetch();
