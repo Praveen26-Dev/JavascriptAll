c556bf002f54844696660937f37dfd2e78ed89ab
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-12 16:08:08 +0530

    Date Object

--- day23/index.js
+++ day23/index.js
@@ -2,4 +2,6 @@ let ran=(Math.round(Math.random()*10))
 console.log(ran)
 
 let d=new Date()
-console.log(d.getMonth())
+console.log(d.getMonth()+1)
+console.log(d.toLocaleDateString())
+console.log(d.toLocaleTimeString())
