c72b87f0f6650f62b7e537b27f80285d5d301eda
Praveen26-Dev <praveenpandey709@gmail.com>
2025-11-13 14:11:48 +0530

    Practise of form validation

---
+++ FormPractice/index.js
@@ -0,0 +1,42 @@
+
+
+document.querySelector("#naspan");
+document.querySelector("#emspan");
+document.querySelector("#mospan");
+document.querySelector("#paspan");
+
+
+
+
+function fun(){
+    let ename=document.querySelector("#name").value
+
+    let email=document.querySelector("#email").value
+
+    let mob=document.querySelector("#mob").value
+
+    let pass=document.querySelector("#pass").value
+
+     if(ename==''){
+      document.querySelector("#naspan").innerHTML="<i>Enter Name</i>"
+      document.querySelector('#name').focus()
+      return false;
+    } 
+    else if(email==''){
+        document.querySelector("#emspan").innerHTML="Enter email"
+        document.querySelector('#email').focus()
+        return false;
+    }
+    else if(!email.includes("@gamil.com")){
+        document.querySelector("#emspan").innerHTML="<i>Enter Correct email</i>"
+        document.querySelector('#email').focus()
+        return false;
+    }
+
+    else if(pass.match(/[!@#$%^&*()+]/)){
+   document.querySelector("#paspan").innerHTML="Enter Special Character ";
+    document.querySelector('#paspan').focus()
+  return false
+    }
+    return false;
+}
